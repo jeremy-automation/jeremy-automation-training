965e2749424997da1b6b1a369a20a52fb9cfa648
jeremyflores <jeremyflores.de@gmail.com>
2025-08-14 09:41:48 +0200

    Remove separate functions

--- src/main/kotlin/Assignment15.kt
+++
@@ -1,9 +0,0 @@
-package org.example
-
-fun main() {
-
-    var messageCount: Int = 1
-    messageCount++
-
-    println(messageCount)
-}
--- src/main/kotlin/Assignment6.kt
+++
@@ -1,9 +0,0 @@
-package org.example
-
-//Assignment 6: Remainder Operator (%)
-fun main(){
-    var num = 25
-    var isEven = num % 2 == 0;
-
-    println("is $num even?: $isEven")
-}
